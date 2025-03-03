069182005be213942631e6711b50cda3b57949af
dani2318 <giacche.dani@gmail.com>
2025-03-03 10:01:33 +0100

    edited build_scripts/toolchain.mk

--- build_scripts/toolchain.mk
+++ build_scripts/toolchain.mk
@@ -10,11 +10,9 @@ export TARGET_LD = $(TARGET)-gcc
 BINUTILS_BUILD = toolchain/build-binutils-$(BINUTILS_VER)
 
 
-toolchain_binutils:
-	mkdir toolchain
-	cd toolchain && wget $(BINUTILS_URL)
+toolchain_binutils: toolchain/binutils-$(BINUTILS_VER).tar.gz
 	cd toolchain && tar -xf binutils-$(BINUTILS_VER).tar.gz
-	mkdir $(BINUTILS_BUILD)
+	mkdir -p $(BINUTILS_BUILD)
 	cd $(BINUTILS_BUILD) && ../binutils-$(BINUTILS_VER)/configure \
 		--prefix=$(TOOLCHAIN_PREFIX) \
 		--target=$(TARGET) \
@@ -25,13 +23,15 @@ toolchain_binutils:
 		$(MAKE) -j4 -C $(BINUTILS_BUILD)
 		$(MAKE) -C $(BINUTILS_BUILD) install
 
+toolchain/binutils-$(BINUTILS_VER).tar.gz:
+	mkdir -p toolchain
+	cd toolchain && wget $(BINUTILS_URL)
+
 GCC_BUILD = toolchain/build-gcc-$(GCC_VER)
 
-toolchain_gcc: toolchain_binutils
-
-	cd toolchain && wget $(GCC_URL)
+toolchain_gcc: toolchain_binutils toolchain/gcc-$(GCC_VER).tar.gz
 	cd toolchain && tar -xf gcc-$(GCC_VER).tar.gz
-	mkdir $(GCC_BUILD) 
+	mkdir -p $(GCC_BUILD) 
 	cd $(GCC_BUILD)  && ../gcc-$(GCC_VER)/configure \
 		--prefix=$(TOOLCHAIN_PREFIX) \
 		--target=$(TARGET) \
@@ -41,3 +41,7 @@ toolchain_gcc: toolchain_binutils
 
 	$(MAKE) -j4 -C $(GCC_BUILD) all-gcc all-target-libgcc
 	$(MAKE) -C $(GCC_BUILD) install-target-libgcc
+
+toolchain/gcc-$(GCC_VER).tar.gz:
+	mkdir -p toolchain
+	cd toolchain && wget $(GCC_URL)
