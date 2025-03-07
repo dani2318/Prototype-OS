0cec4bbf158004868f0baa4e45487e51c2955f23
dani2318 <70331791+dani2318@users.noreply.github.com>
2025-03-07 20:28:39 +0100

    Switching from makefile to scons (#9)
    * Switching to scone
    
    * removed unused files
    
    * removed unused files
    
    * Compleated scons build system integration, missing to fix stage2 in the disk image generated!
    
    * download toolchain as sh file
    
    * ended SConscript for the os img

--- run.ps1
+++
@@ -1,2 +0,0 @@
-qemu-system-i386.exe -fda \\wsl.localhost\Debian\home\dani23\os\build\main_floppy.img
-pause
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -6,6 +6,7 @@
 
 extern uint8_t __bss_start;
 extern uint8_t __end;
+extern void _init();
 
 void timer(Registers* regs){
   printf(".");
@@ -14,6 +15,8 @@ void timer(Registers* regs){
 void __attribute__((section(".entry"))) start(uint16_t bootDrive){
 
   memset(&__bss_start,0,(&__end) - (&__bss_start));
+  _init(); // call global constr.
+
   clscr();
 
   printf("============       oskrnl loading!       ============\n");
